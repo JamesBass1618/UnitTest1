7554ccd4bba6361624c5960970cae4d4b3e5e96d
James Bass <jamesbass16180@gmail.com>
2024-06-07 09:48:57 -0500

    Adding Google test

--- UnitTest1/UnitTest1.cs
+++ UnitTest1/UnitTest1.cs
@@ -1,14 +1,49 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
 
-namespace UnitTest1
+namespace GoogleUITest
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSearchStreetFighterVThenVerifyStreetFighterVIsDisplayed()
         {
+            int waitingTime = 2000;
+            By googleSearchBar = By.Name("q");
+            By googleSearchButton = By.Name("btnK");
+            By googleResultText = By.XPath(".//h2//span[text()='Street Fighter V']");
+            By googleIAgreeButton = By.Id("L2AGLb");
+
+            IWebDriver webDriver = new ChromeDriver();
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.Navigate().GoToUrl("https://www.google.co.uk");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleIAgreeButton).Click();
+
+            webDriver.Manage().Window.Maximize();
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchBar).SendKeys("Street Fighter V");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchButton).Click();
+
+            Thread.Sleep(waitingTime);
+
+            var actualResultText = webDriver.FindElement(googleResultText);
+
+            Assert.IsTrue(actualResultText.Text.Equals("Street Fighter V"));
+
+            webDriver.Quit();
         }
     }
 }
