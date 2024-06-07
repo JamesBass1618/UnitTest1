1a24b341a8053d9c5ec698e5fb68289a8ea78b60
James Bass <jamesbass16180@gmail.com>
2024-06-07 09:42:16 -0500

    Add project files.

---
+++ UnitTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Coretelligent")]
+[assembly: AssemblyProduct("UnitTest1")]
+[assembly: AssemblyCopyright("Copyright © Coretelligent 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cb1317cc-3c60-4205-98f4-8b6d559a3306")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
